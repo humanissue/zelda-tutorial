e2b56bf9d6de24bd175e24fb07a088c46ee2f2b9
humanissue <48816092+humanissue@users.noreply.github.com>
2019-03-31 17:01:11 +1100

    lesson 9

---
+++ Zelda Tutorial/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D myRigidBody;
+    private Vector3 change;
+    private Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        updateAnimationAndMove();
+    }
+
+    void moveCharacter()
+    {
+        myRigidBody.MovePosition(transform.position + (change*speed*Time.deltaTime));
+    }
+
+    void updateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
+            moveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+}
---
+++ Zelda Tutorial/Assets/Scripts/roomMove.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class roomMove : MonoBehaviour
+{
+    public Vector2 cameraChange;
+    public Vector3 playerChange;
+    private cameraMovement cam;
+    public bool namedPlace;
+    public string placeName;
+    public GameObject text;
+    public Text placeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.GetComponent<cameraMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            cam.minPosition += cameraChange;
+            cam.maxPosition += cameraChange;
+            other.transform.position += playerChange;
+            if(namedPlace)
+            {
+                StartCoroutine(PlaceNameCo());
+            }
+        }
+    }
+
+    private IEnumerator PlaceNameCo()
+    {
+        text.SetActive(true);
+        placeText.text = placeName;
+        yield return new WaitForSeconds(4f);
+        text.SetActive(false);
+    }
+}
